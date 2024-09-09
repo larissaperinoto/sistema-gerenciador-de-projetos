3281cbb7741304c11c97223d8c85369309d02a52
Larissa Perinoto <perinotolarissa@gmail.com>
2024-09-09 10:26:00 -0300

    feat: cria middleware genérico para validação

---
+++ gerenciamento-projetos/backend/src/middlewares/validator.middleware.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from "express";
+import { validateOrReject } from "class-validator";
+import { httpStatus } from "../utils/httpStatus";
+
+export async function validatorMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+  validatorSchema: any
+) {
+  try {
+    const measure = new validatorSchema(req.body);
+
+    await validateOrReject(measure);
+
+    next();
+  } catch (e: any) {
+    const description = Object.values(e[0].constraints)[0];
+    res.status(httpStatus.BadRequest).send({
+      message: description,
+    });
+  }
+}
