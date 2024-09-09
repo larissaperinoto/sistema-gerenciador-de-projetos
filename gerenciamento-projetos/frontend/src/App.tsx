d8209c01ca49602c6b6c2a0f4fca23f4a01b506c
Larissa Perinoto <perinotolarissa@gmail.com>
2024-09-09 18:56:38 -0300

    feat: cria rota de login

--- gerenciamento-projetos/frontend/src/App.tsx
+++ gerenciamento-projetos/frontend/src/App.tsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Main } from "./layouts/Main/Main";
+import { Login } from "./components/Login/Login";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Main content={<Login />} />} />
+    </Routes>
+  );
 }
 
 export default App;
