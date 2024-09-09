c0cf63edfe4ea77545242445fb6c09a2837f9503
Larissa Perinoto <perinotolarissa@gmail.com>
2024-09-09 19:31:30 -0300

    feat: insere rota de registro de usuários

--- gerenciamento-projetos/frontend/src/App.tsx
+++ gerenciamento-projetos/frontend/src/App.tsx
@@ -2,11 +2,13 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { Main } from "./layouts/Main/Main";
 import { Login } from "./components/Login/Login";
+import { Register } from "./components/Register/Register";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Main content={<Login />} />} />
+      <Route path="/register" element={<Main content={<Register />} />} />
     </Routes>
   );
 }
