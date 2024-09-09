4ed1c567f2370101678491ee625aacb417204efa
Larissa Perinoto <perinotolarissa@gmail.com>
2024-09-09 10:25:43 -0300

    feat: cria schemas de validação para usuários com class-validator

---
+++ gerenciamento-projetos/backend/src/utils/class-validator/auth.validator.ts
@@ -0,0 +1,51 @@
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsString,
+  IsStrongPassword,
+} from "class-validator";
+import { RegisterType, RoleTypes } from "../types/register";
+
+export class LoginValidatorSchema {
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsStrongPassword(
+    {
+      minLength: 8,
+      minUppercase: 1,
+      minLowercase: 1,
+      minSymbols: 1,
+      minNumbers: 1,
+    },
+    {
+      message:
+        "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.",
+    }
+  )
+  password: string;
+
+  constructor(login: { email: string; password: string }) {
+    this.email = login.email;
+    this.password = login.password;
+  }
+}
+
+export class RegisterUserValidatorSchema extends LoginValidatorSchema {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsEnum(RoleTypes)
+  role: string;
+
+  constructor(register: RegisterType) {
+    super(register);
+    this.name = register.name;
+    this.role = register.role;
+  }
+}
