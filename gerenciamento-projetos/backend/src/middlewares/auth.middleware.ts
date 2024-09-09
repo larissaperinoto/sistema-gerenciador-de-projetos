f2ea39d54cf9541c3a50bbcc31316dad0f19fcad
Larissa Perinoto <perinotolarissa@gmail.com>
2024-09-09 10:38:27 -0300

    feat: cria middleware para decodificação do JWT

---
+++ gerenciamento-projetos/backend/src/middlewares/auth.middleware.ts
@@ -0,0 +1,26 @@
+import * as jwt from "jsonwebtoken";
+import { RequestHandler } from "express";
+import { httpStatus } from "../utils/httpStatus";
+
+const authMiddleware: RequestHandler = async (req, res, next) => {
+  const { authorization } = req.headers;
+  console.log(authorization);
+
+  if (!authorization)
+    return res
+      .status(httpStatus.BadRequest)
+      .json({ message: "Token not found." });
+
+  try {
+    const user = jwt.verify(authorization, process.env.JWT_SECRET as string);
+    console.log(user);
+    req.body = user;
+    next();
+  } catch (error) {
+    return res
+      .status(httpStatus.BadRequest)
+      .json({ message: "Token must be a valid token." });
+  }
+};
+
+export default authMiddleware;
