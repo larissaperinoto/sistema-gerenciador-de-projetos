5fcaf41a2f5bf94f602154ef5f489cce98d5c347
Larissa Perinoto <perinotolarissa@gmail.com>
2024-09-10 17:55:25 -0300

    feat: cria service para requisicoes users

---
+++ gerenciamento-projetos/frontend/src/services/users.service.ts
@@ -0,0 +1,29 @@
+import { fetchData, HttpMethod } from "../utils/fetch";
+
+export class UsersService {
+  private _serverUrl: string;
+  static instance: UsersService;
+
+  constructor(serverUrl: string) {
+    this._serverUrl = serverUrl;
+  }
+
+  static getInstance() {
+    if (!this.instance) {
+      this.instance = new UsersService(
+        process.env.REACT_APP_SERVER_URL as string
+      );
+    }
+
+    return this.instance;
+  }
+
+  public async getUsers() {
+    const getProjectsPath = process.env.REACT_APP_USERS_PATH as string;
+    const data = await fetchData(
+      this._serverUrl + getProjectsPath,
+      HttpMethod.GET
+    );
+    return data;
+  }
+}
