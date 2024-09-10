99d7be4fb00519346ffcc92dba4c913805ab04d4
Larissa Perinoto <perinotolarissa@gmail.com>
2024-09-10 16:39:53 -0300

    chore: dockeriza aplicações front, back e banco de dados

--- gerenciamento-projetos/backend/src/database/config/database.ts
+++ gerenciamento-projetos/backend/src/database/config/database.ts
@@ -4,9 +4,9 @@ import { Options } from "sequelize";
 const config: Options = {
   username: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: process.env.DB_NAME || "projects_manager",
-  host: process.env.MYSQL_HOST || "localhost",
-  port: Number(process.env.MYSQL_PORT) || 3306,
+  database: process.env.DB_NAME,
+  host: process.env.MYSQL_HOST,
+  port: Number(process.env.MYSQL_PORT),
   dialect: "mysql",
   logging: false,
 };
